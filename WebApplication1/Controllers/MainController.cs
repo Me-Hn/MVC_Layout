d364d4fcde1411a02e9f66b0a6b4622f3f81d6e9
Me-Hn <zzainkhan313@gmail.com>
2025-02-11 00:24:23 +0500

    Add project files.

---
+++ WebApplication1/Controllers/MainController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class MainController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult about()
+        {
+            return View();
+        }
+
+        public IActionResult login()
+        {
+            return View();
+        }
+
+
+
+    }
+}
